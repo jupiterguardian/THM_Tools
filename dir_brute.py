db08946e40c8f158abd59bc293ecb4ce78c899f2
jupiterguardian <jupiterguardian.py@gmail.com>
2022-11-16 19:43:35 -0800

    Update dir_brute.py

--- dir_brute.py
+++ dir_brute.py
@@ -3,19 +3,19 @@ from requests_html import HTMLSession
 session = HTMLSession()
 
 dirs = ['/',
- '/news',
- '/news/article?id=1',
- '/news/article?id=1',
- '/contact',
- '/customers',
- '/customers/login.',
- '/customers/login',
- '/customers/signup',
- '/customers/reset',
- '/customers',
- '/customers/ticket/new',
- '/customers/account',
- '/customers/logout']
+ 'news',
+ 'news/article?id=1',
+ 'news/article?id=1',
+ 'contact',
+ 'customers',
+ 'customers/login.',
+ 'customers/login',
+ 'customers/signup',
+ 'customers/reset',
+ 'customers',
+ 'customers/ticket/new',
+ 'customers/account',
+ 'customers/logout']
 
 def brute_force(target, directory_list):
     for directory in directory_list:
