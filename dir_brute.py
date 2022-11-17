973a36dd5b8058f7bea7c7f34108d7195519143d
kali <jupiterguardian.py@gmail.com>
2022-11-16 22:58:42 -0800

    Removed hardcoded List, added file argument to bruteforce(), stripped newline to avoid false 404 http status code

--- dir_brute.py
+++ dir_brute.py
@@ -2,24 +2,16 @@ from requests_html import HTMLSession
 
 session = HTMLSession()
 
-dirs = ['/',
- '/news',
- '/news/article?id=1',
- '/news/article?id=1',
- '/contact',
- '/customers',
- '/customers/login.',
- '/customers/login',
- '/customers/signup',
- '/customers/reset',
- '/customers',
- '/customers/ticket/new',
- '/customers/account',
- '/customers/logout']
+from requests_html import HTMLSession
 
-def brute_force(target, directory_list):
-    for directory in directory_list:
-                
-        r = session.get(f'{target}/{directory}')
-        print(r)
+session = HTMLSession()
+
+def brute_force(target, directories):
+    with open(directories, 'r') as dirs:
+        for directory in dirs:
+            directory = directory.strip() #avoid false 404 http status
+            request = session.get(f'{target}/{directory}')
+            print(request, f'{target}/{directory}')
+
+
 
